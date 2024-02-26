7a9bbcd90d4812a6ee8cb312be9f65f9464addaa
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-26 16:33:11 -0600

    express login works

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
-const express = require('express');
-
+// const express = require('express');
+// const dotenv = require("dotenv");
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
 app.get("/", (req,res) => {
     res.send('Hello MERN!!')
-})
+});
 
-app.listen(5001, () => console.log('yes Pratik!!, Server is runing on port 5000'));
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => console.log(`yes Pratik!!, Server is runing on port ${PORT}`));
